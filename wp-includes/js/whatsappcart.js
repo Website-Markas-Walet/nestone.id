03eb1f2ebd97b35c11409444f34a0e63538bed37
ShalHafizh <85092772+ShalHafizh@users.noreply.github.com>
2023-05-25 15:06:11 +0700

    Update whatsappcart.js

--- wp-includes/js/whatsappcart.js
+++ wp-includes/js/whatsappcart.js
@@ -2,8 +2,9 @@ function whatsapp(){
     var produk = document.getElementById("produk").value;
     var nama = document.getElementById("nama").value;
     var alamat = document.getElementById("alamat").value;
-    var pcs = document.getElementById("pcs").value;
-    var total = document.getElementById("total").value;
+    var pcs = document.getElementById("pcs");
+    var nilaiPcs = pcs.textContent;
+    var harga = document.getElementById("harga").value;
     var provinsi = document.getElementById("provinsi").value;
     var kota = document.getElementById("kota").value;
     var kecamatan = document.getElementById("kecamatan").value;
@@ -12,11 +13,11 @@ function whatsapp(){
 
     var whatsappurl="https://wa.me/6287725260196?text="
     + "Halo nestone.id, Saya" + "%20" + nama + "%0a"
-    + "ingin memesan produk" + "%20" + produk + "%0a"
-    + "Total pcs :" + "%20" + pcs + "%0a"
-    + "Total Harga : Rp." + total + "%0a"
     + "Alamat :" + "%20" + kelurahan + "%20" + alamat + ", "+kecamatan+", "+kota+", "+provinsi + "%0a"
-    + "Kode Pos : " + pos;
+    + "Kode Pos : " + pos + "%0a" + "%0a"
+    + "Ingin memesan produk : " + "%20" + produk + "%0a"
+    + "Total pcs :" + "%20" + nilaiPcs + "%0a"
+    + "Harga Satuan Produk : Rp." + harga;
 
     window.open(whatsappurl,"_blank").focus();
-} 
+}
